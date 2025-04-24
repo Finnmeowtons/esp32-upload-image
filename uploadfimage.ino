745dec7efde01a0f46d1a716f9820b69993f436a
Finnmeowtons <soriano.christianjose.m@gmail.com>
2025-04-25 03:03:26 +0800

    removed streaming

--- uploadfimage.ino
+++ uploadfimage.ino
@@ -158,60 +158,6 @@ void sendPhotoToServer() {
 }
 //________________________________________________________________________________
 
-//________________________________________________________________________________  Stream Handler
-// MJPEG stream handler
-static esp_err_t stream_handler(httpd_req_t *req) {
-  camera_fb_t *fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t *_jpg_buf = NULL;
-  char *part_buf[64];
-
-  static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=123456789000000000000987654321";
-  static const char *_STREAM_BOUNDARY = "\r\n--123456789000000000000987654321\r\n";
-  static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if (res != ESP_OK) return res;
-
-  while (true) {
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      httpd_resp_send_500(req);
-      return ESP_FAIL;
-    }
-
-    if (fb->format != PIXFORMAT_JPEG) {
-      bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-      esp_camera_fb_return(fb);
-      if (!jpeg_converted) {
-        httpd_resp_send_500(req);
-        return ESP_FAIL;
-      }
-    } else {
-      _jpg_buf = fb->buf;
-      _jpg_buf_len = fb->len;
-    }
-
-    res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    if (res == ESP_OK) {
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if (res == ESP_OK) {
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-
-    if (fb->format != PIXFORMAT_JPEG) free(_jpg_buf);
-    esp_camera_fb_return(fb);
-
-    if (res != ESP_OK) break;
-  }
-
-  return res;
-}
-//________________________________________________________________________________
-
 //________________________________________________________________________________sendIpAddress()
 void sendIpAddress() {
   http.begin("http://157.245.204.46:3001/api/register-ip");
@@ -338,31 +284,11 @@ void setup() {
   Serial.println();
   Serial.println("Set camera ESP32 CAM successfully.");
   //----------------------------------------
-  startCameraServer();
   sendIpAddress();
   Serial.println();
   Serial.print("ESP32-CAM captures and sends photos to the server every 60 seconds.");
 }
 //________________________________________________________________________________
-
-//________________________________________________________________________________ // Starts HTTP server with stream handler
-void startCameraServer() {
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
-  httpd_uri_t stream_uri = {
-    .uri = "/stream",
-    .method = HTTP_GET,
-    .handler = stream_handler,
-    .user_ctx = NULL
-  };
-
-  if (httpd_start(&server, &config) == ESP_OK) {
-    httpd_register_uri_handler(server, &stream_uri);
-    Serial.println("Wi-Fi connected: " + WiFi.localIP().toString());
-  }
-}
-//________________________________________________________________________________
-
 
 
 //________________________________________________________________________________ VOID LOOP()
